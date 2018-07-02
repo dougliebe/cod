6907da42eafeed7d1b16ec8aadbe0c5f6432d7ab
dougliebe <liebe.doug@gmail.com>
2018-07-01 22:47:30 -0400

    correct for average kills of player

--- files/r-files/killdeathimp.R
+++ files/r-files/killdeathimp.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(ggplot2)
-setwd("~/Documents/CoD/full-game-data")
+setwd("C:/Users/Doug/Documents/CoD/cwl-data/data")
 # read in the csv
 filenames <- list.files( pattern="*.csv", full.names=TRUE)
 
@@ -8,7 +8,7 @@ output <- data.frame()
 
 for (i in 1:(length(filenames)-2)) {
   data <- read.csv(filenames[i])
-  data <- subset(data, data$mode == 'Search & Destroy')
+  data <- subset(data, data$mode == 'Hardpoint')
   output <- rbind(output, data)
 }
 
@@ -41,19 +41,20 @@ output %>%
   group_by(team,player) %>%
   mutate(sub = ifelse(fave.weapon == 'PPSh-41',1,0)) %>%
   mutate(win = ifelse(win. == "W",1,0)) %>%
-  summarise(kill.imp = lm(score ~ kills + deaths)$coefficients[2],
-            death.imp = lm(score ~ kills + deaths)$coefficients[3],
-            sub = ifelse(mean(sub)>0.8,'sub', 'AR/flex'), n = n()) %>%
+  summarise(kill.imp = lm(score ~ kills + deaths)$coefficients[2]*mean(kills),
+            death.imp = lm(score ~ kills + deaths)$coefficients[3]*mean(deaths),
+            subs = ifelse(mean(sub)>0.5,'sub', 'AR/flex'),
+            sub_freq = mean(sub), n = n(), kills = mean(kills), deaths = mean(deaths)) %>%
   ungroup() %>%
-  # group_by(team, sub) %>%
-  # mutate(kr = kill.imp-mean(kill.imp), dr = death.imp-mean(death.imp)) %>%
-  mutate(kpd = (kill.imp-1) + abs(death.imp-1)) %>%
+  group_by(team, subs) %>%
+  mutate(kr = kill.imp-mean(kill.imp), dr = death.imp-mean(death.imp)) %>%
+  # mutate(kpd = (kill.imp-1) + abs(death.imp-1)) %>%
   filter(n > 20) %>%
   # filter(sub == 'sub') %>%
   # filter(team == "Enigma6") %>%
   # group_by(team) %>%
-  # summarise(kpd = max(kpd)-mean(kpd)) %>%
-  arrange(desc(kpd))
+  # mutate(kpd = kr-mean(kr)) %>%
+  arrange(desc(kr))
 
 
 # Look at some players
