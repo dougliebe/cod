3d67e155c7e8f31f9c25ca01be3eb94fa6ca5001
dougliebe <liebe.doug@gmail.com>
2018-07-05 17:35:36 -0400

    updates

--- files/r-files/team-wl-record.R
+++ files/r-files/team-wl-record.R
@@ -6,17 +6,15 @@ filenames <- list.files( pattern="*.csv", full.names=TRUE)
 
 output <- data.frame()
 
-for (i in 1:(length(filenames))) {
+for (i in (length(filenames)-1):(length(filenames))) {
   data <- read.csv(filenames[i])
-  data <- subset(data, data$mode == 'Hardpoint')
+  data <- subset(data, data$mode == 'Hardpoint') # sort by mode
   output <- rbind(output, data)
 }
 
-getmode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
 output %>%
-  group_by(team) %>%
   mutate(win = ifelse(win. == "W",1,0)) %>%
-  summarise(wins = mean(win))
+  group_by(team) %>%
+  summarise(wins = mean(win), W = sum(win), L = n()-W, n = n()) %>%
+  filter(team == "Team Kaliber") %>%
+  arrange(desc(wins))
